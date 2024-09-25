41bef28808203bdbf0e76d3e39f3081d41585197
Maxime <maxime.celeste1221@gmail.com>
2024-09-25 15:22:20 +0400

    start CRUD methods product routes

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
 
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // allow you to parse the body of the request
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on http://localhost:" + PORT);
