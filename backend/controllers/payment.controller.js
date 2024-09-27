62de10d46cbef5c719cf3da3fb97adbbee3b31ce
Maxime <maxime.celeste1221@gmail.com>
2024-09-27 13:52:39 +0400

    add payment methods

---
+++ backend/controllers/payment.controller.js
@@ -0,0 +1,111 @@
+import { stripe } from "../lib/stripe.js";
+
+import Coupon from "../models/coupon.model.js";
+
+export const createCheckoutSession = async (req, res) => {
+  try {
+    const { product, couponCode } = req.body;
+
+    if (!Array.isArray(product) || !product.length === 0) {
+      return res.status(400).json({ error: "Invalir or empty product array" });
+    }
+
+    let totalAmount = 0;
+
+    const lineItems = product.map((product) => {
+      const amount = Math.round(product.price * 100);
+      totalAmount += amount * product.quantity;
+
+      return {
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: product.name,
+            images: [product.image],
+          },
+          unit_amount: amount,
+        },
+      };
+    });
+
+    let coupon = null;
+    if (couponCode) {
+      coupon = await Coupon.findOne({
+        code: couponCode,
+        userId: req.user._id,
+        isActive: true,
+      });
+      if (coupon) {
+        totalAmount -= Math.round(
+          (totalAmount * coupon.discountPercentage) / 100
+        );
+      }
+    }
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: lineItems,
+      mode: "payment",
+      success_url: `${process.env.CLIENT_URL}/purshase-success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.CLIENT_URL}/purshase-cancel`,
+      discounts: coupon
+        ? [
+            {
+              coupon: await createStripeCoupon(coupon.discountPercentage),
+            },
+          ]
+        : [],
+      metadata: {
+        userId: req.user._id.toString(),
+        couponCode: couponCode || "",
+      },
+    });
+
+    if (totalAmount >= 20000) {
+      await createNewCoupon(req.user._id);
+    }
+    res.status(200).json({ id: session.id, totalAmount: totalAmount / 100 });
+  } catch (error) {}
+};
+
+export const checkoutSuccess = async (req, res) => {
+  try {
+    const { sessionId } = req.body;
+    const session = await stripe.checkout.sessions.retrieve(sessionId);
+
+    if (session.payment_status === "paid") {
+      if (session.metadata.couponCode) {
+        await Coupon.findOneAndUpdate(
+          {
+            code: session.metadata.couponCode,
+            userId: session.metadata.userId,
+          },
+          {
+            isActive: false,
+          }
+        );
+      }
+    }
+  } catch (error) {}
+};
+
+async function createStripeCoupon(discountPercentage) {
+  const coupon = await stripe.coupons.create({
+    percent_off: discountPercentage,
+    duration: "once",
+  });
+  return coupon.id;
+}
+
+async function createNewCoupon(userId) {
+  const newCoupon = new Coupon({
+    code: "GIFT" + Math.random().toString(36).substring(2, 8).toUpperCase(),
+    discountPercentage: 10,
+    expirationDate: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // 30 days from now
+    userId: userId,
+  });
+
+  await newCoupon.save();
+
+  return newCoupon;
+}
